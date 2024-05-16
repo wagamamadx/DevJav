c0001d1b721fd8cae4ef5a4de9d27f810b400863
Raul Abad Pezo Olivera <rapezo@ayesa.es>
2024-05-16 13:05:15 -0500

    develop 3

--- Colecciones2/List/src/App.java
+++ Colecciones2/List/src/App.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Vector;
+// import java.util.Vector;
 
 
 public class App {
